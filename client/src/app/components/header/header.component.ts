a0fa2fa36af9039890c4871a2b18ffabc5803b6d
Rohit Tewari <rtewari056@gmail.com>
2023-11-26 23:13:31 +0530

    Add bootstrap

---
+++ client/src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, MatToolbarModule, MatButtonModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+}
